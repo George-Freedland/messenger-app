1e89ebe9ad4cd4173d4b64955e2fd4159b1ca8fd
George Freedland <g.freedland@ventra.ru>
2020-01-29 17:59:46 -0800

    organized front and back

--- frontend/messengr/src/App.js
+++ frontend/messengr/src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor: "#4378"}}>
+      <div style={{textAlign: "left"}}>Messengr</div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Enter your name: </p>
+        <input></input>
+        <br />
+        <button type="submit">Click Me</button>
       </header>
     </div>
   );
